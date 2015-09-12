d560e6d989f0d9a87d17bc5913e204e1f1992712
kfirzuberi <kfirzuberi@gmail.com>
2015-09-13 01:17:33 +0300

    feat(server): use Lout and Joi plugins

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var Hapi = require('hapi');
 var net = require('net');
+var Joi = require('joi');
+var Inert = require("inert");
+var Vision = require("vision");
 
 var port = process.env.PORT || 3000;
 var server = new Hapi.Server();
 server.connection({ port: port });
+server.register([Inert,Vision, require('lout') ], function(err) {
+
+});
 
 server.route({
     method: 'POST',
@@ -29,12 +35,23 @@ server.route({
             client.destroy(); // kill client after server's response
         });
 
-
         // handle on connection close.
         client.on('close', function() {
             console.log('Connection closed');
             reply('request finished successfully'); // reply to the client and finish the request.
         });
+    },
+    config: {
+        validate: {
+            payload: {
+                port: Joi.number().integer().min(999).max(9999).positive().description('server port').example('1337').default('1337'),
+                message: Joi.string().min(5).max(20).description('message to send').example('X=32&Y=31').default('X=32&Y=31'),
+                hostName: Joi.string().ip({version :'ipv4'}).description('server host name').example('127.0.0.1').default('127.0.0.1')
+            }
+        },
+        description: 'Send TCP message',
+        notes: 'Send the given message to the given host name via TCP',
+        tags: ['api']
     }
 });
 
