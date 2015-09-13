6336809468505c683f3a7bf811bf8981d3c4837a
kfirzuberi <kfirzuberi@gmail.com>
2015-09-13 11:20:41 +0300

    feat(server): Add log using Good Close #5 feat(server): Add error handling using Boom

--- web.js
+++ web.js
@@ -3,11 +3,31 @@ var net = require('net');
 var Joi = require('joi');
 var Inert = require("inert");
 var Vision = require("vision");
+var Boom = require('boom');
+
+var goodOptions = {
+    opsInterval: 1000,
+    reporters: [{
+        reporter: require('good-console'),
+        events: {log: '*', response: '*', error: '*' }
+    }, {
+        reporter: require('good-file'),
+        events: {log: '*', response: '*', error: '*'},
+        config: './logs/log'
+    }]
+};
 
 var port = process.env.PORT || 3000;
 var server = new Hapi.Server();
 server.connection({ port: port });
-server.register([Inert,Vision, require('lout') ], function(err) {
+server.register([Inert,Vision, require('lout')], function(err) {
+
+});
+
+server.register({
+    register: require('good'),
+    options: goodOptions
+}, function (err) {
 
 });
 
@@ -16,29 +36,34 @@ server.route({
     path: '/httpToTcp',
     handler: function (request, reply) {
         // get body parameters - server name, port and message to send.
-        var port = request.payload.port;
-        var hostName = request.payload.hostName;
+        var clientPort = request.payload.port;
+        var clientHostName = request.payload.hostName;
         var message = request.payload.message;
 
         // create new socket to the given client parameters.
         var client = new net.Socket();
-        client.connect(port, hostName, function() {
-            console.log('Connected');
+        client.connect(clientPort, clientHostName, function() {
+            server.log(['info'],'Connected to ' + clientHostName + ':' + clientPort);
 
             // send the message to the client.
             client.write(message);
+            reply('request finished successfully. Send - "' + message + '"'); // reply to the client and finish the request.
         });
 
         // handle on data receive from the server.
         client.on('data', function(data) {
-            console.log('Received: ' + data);
+            server.log(['info'],'Received: ' + data);
             client.destroy(); // kill client after server's response
         });
 
         // handle on connection close.
         client.on('close', function() {
-            console.log('Connection closed');
-            reply('request finished successfully'); // reply to the client and finish the request.
+            server.log(['info'],'Connection closed');
+        });
+
+        client.on('error', function(err){
+            server.log(['error'], 'Error: ' + err.message);
+            reply(Boom.forbidden('Couldn\'t connect to ' + clientHostName + ':' + clientPort));
         });
     },
     config: {
@@ -57,5 +82,5 @@ server.route({
 
 // start the server.
 server.start(function () {
-    console.log('Server running at:', server.info.uri);
+    server.log(['info'],'Server running at:' + server.info.uri);
 });
